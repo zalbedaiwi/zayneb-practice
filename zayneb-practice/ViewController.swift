c03396ce3cb453183a244bfbec1f0606b70f58b8
Zayneb Al-Bedaiwi <princesszayneb@gmail.com>
2021-06-29 13:44:58 -0500

    practice Xcode

---
+++ zayneb-practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  zayneb-practice
+//
+//  Created by  Scholar on 6/29/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
